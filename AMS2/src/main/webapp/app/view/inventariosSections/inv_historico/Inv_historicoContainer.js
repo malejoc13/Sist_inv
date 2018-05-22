dd9ba78ab0cd1702e5424bf46b5d9e61cb189983
malejoc13 <34068045+malejoc13@users.noreply.github.com>
2018-05-22 13:46:06 -0500

    modificando para que se refresque al visualizar el historico

--- AMS2/src/main/webapp/app/view/inventariosSections/inv_historico/Inv_historicoContainer.js
+++ AMS2/src/main/webapp/app/view/inventariosSections/inv_historico/Inv_historicoContainer.js
@@ -1,6 +1,11 @@
 Ext.define('Admin.view.inventariosSections.inv_historico.Inv_historicoContainer', {
     extend: 'Admin.base.BaseContainer',
     xtype: 'inv_historicos',
+    listeners: {
+        show : function ( me, eOpts ) {            
+            me.items.items[0].items.items[0].getStore().loadPage(1);
+        }
+    },
     items: [
         {
             xtype: 'inv_historicoGridTab'
--- AMS2/src/main/webapp/app/view/inventariosSections/inv_historico/grid/Inv_historicoGridTab.js
+++ AMS2/src/main/webapp/app/view/inventariosSections/inv_historico/grid/Inv_historicoGridTab.js
@@ -5,6 +5,7 @@ Ext.define('Admin.view.inventariosSections.inv_historico.grid.Inv_historicoGridT
     entity:'inv_historico',
     filters:'inv_historico.id',
     staticFilters:'unidad.id',//para listar inventarios solo de la unidad del usuario autenticado
+    
     items: [ 
         {
             xtype: 'inv_historicoGrid' //item 0 de grid tab           
